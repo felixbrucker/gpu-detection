95143b9c4afc314200890acf72237219dc45482b
Felix Brucker <felix@felixbrucker.com>
2021-03-06 00:18:50 +0100

    Prepare release v0.1.5

--- bin/build-package.js
+++ bin/build-package.js
@@ -4,14 +4,15 @@ const mkdirp = require('mkdirp');
 const archiver = require('archiver');
 
 (async () => {
-  const buildDir = 'build';
-  const nativeModulePath = join('native', 'gpu-detection.node');
+  const buildDir = join(__dirname, '..', 'build');
+  const nativeModulePathInZip = join('native', 'gpu-detection.node');
+  const nativeModulePath = join(__dirname, '..', nativeModulePathInZip);
   mkdirp.sync(buildDir);
   const zipFileStream = createWriteStream(join(buildDir, `${process.platform}-${process.arch}.tar.gz`));
   const zipFileClosedPromise = new Promise(resolve => zipFileStream.once('close', resolve));
   const archive = archiver('tar', { gzip: true, gzipOptions: { level: 1 } });
   archive.pipe(zipFileStream);
-  archive.append(createReadStream(nativeModulePath), { name: nativeModulePath });
+  archive.append(createReadStream(nativeModulePath), { name: nativeModulePathInZip });
   await archive.finalize();
   await zipFileClosedPromise;
 })();
--- bin/rename-native-module.js
+++ bin/rename-native-module.js
@@ -1,6 +1,6 @@
 const { renameSync } = require('fs');
 const { join } = require('path');
 
-const nativeDir = join(__dirname, 'native');
+const nativeDir = join(__dirname, '..', 'native');
 
 renameSync(join(nativeDir, 'index.node'), join(nativeDir, 'gpu-detection.node'));
